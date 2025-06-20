bebd29ac0c88e74fcc940fb1315133fa3ff80c93
Blaise <blazzze0517@gmail.com>
2025-06-19 21:19:58 -0400

    Book Analysis functionality added.

--- app.py
+++ app.py
@@ -208,8 +208,17 @@ def app():
                     print('Book Deleted!')
                     time.sleep(1)
         elif choice == '4':
-            # analysis
-            pass
+            oldest_book = session.query(Book).order_by(Book.published_date).first()
+            newest_book = session.query(Book).order_by(Book.published_date.desc()).first()
+            total_books = session.query(Book).count()
+            python_books = session.query(Book).filter(Book.title.like('%Python%')).count()
+            print(f'''
+                \n******* BOOK ANALYSIS *******
+                \rOldest Book: {oldest_book}
+                \rNewest Book: {newest_book}
+                \rTotal Books: {total_books}
+                \rNumber of Python Books: {python_books}''')
+            input('\nPress Enter to return to Main Menu.')
         else:
             print('Goodbye...')
             app_running = False
